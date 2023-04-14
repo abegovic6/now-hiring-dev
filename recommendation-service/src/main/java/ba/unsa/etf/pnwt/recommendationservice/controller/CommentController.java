e51ee8c759681ae1dcdc737db973e60d79e85a3f
Senija <skaleta1@etf.unsa.ba>
2023-04-14 15:40:17 +0200

    added comment controller

---
+++ recommendation-service/src/main/java/ba/unsa/etf/pnwt/recommendationservice/controller/CommentController.java
@@ -0,0 +1,42 @@
+package ba.unsa.etf.pnwt.recommendationservice.controller;
+
+import ba.unsa.etf.pnwt.recommendationservice.dto.CommentEntitryHelper;
+import ba.unsa.etf.pnwt.recommendationservice.entity.CommentEntity;
+import ba.unsa.etf.pnwt.recommendationservice.entity.RecommendationEntity;
+import ba.unsa.etf.pnwt.recommendationservice.service.CommentServiceImp;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/comment")
+public class CommentController {
+    private final CommentServiceImp commentService;
+
+    @Autowired
+    public CommentController(CommentServiceImp commentService) {
+        this.commentService = commentService;
+    }
+
+    @GetMapping
+    public List<CommentEntity> getAllComments(){
+        return commentService.getAllComments();
+    }
+    @GetMapping(path = "/byUser")
+    public List<CommentEntity> getCommentsByUSer(@RequestParam(required = true) Long id){
+        return commentService.getCommentByUser(id);
+    }
+
+    @GetMapping(path = "/byUserEmail")
+    public List<CommentEntity> getCommentsByUserEmail(@RequestParam(required = true) String email){
+        return commentService.getCommentByUserEmail(email);
+    }
+    @PostMapping(path="/addComment")
+    public ResponseEntity<CommentEntity> addComment(@Valid @RequestBody CommentEntitryHelper comment){
+        CommentEntity addedComment = commentService.addNewComment(comment);
+        return ResponseEntity.ok(addedComment);
+    }
+}

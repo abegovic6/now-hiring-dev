b1bc3f3908f703ae1ac1ae4ac97f5546d7c81270
Amila Begovic <abegovic6@etf.unsa.ba>
2023-05-02 18:56:57 +0200

    GRPC added

---
+++ grpc/src/main/java/ba/unsa/etf/pnwt/grpc/services/LoggingService.java
@@ -0,0 +1,10 @@
+package ba.unsa.etf.pnwt.grpc.services;
+
+import ba.unsa.etf.pnwt.proto.LoggingServiceGrpc;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class LoggingService extends LoggingServiceGrpc {
+
+   
+}
